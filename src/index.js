51a3af8f1e2d0a600d4a5f2a75c7d07e08821698
Duane <duanerichard47@hotmail.com>
2023-01-22 22:19:25 -0500

    woof-woof CC incomplete

--- src/index.js
+++ src/index.js
@@ -1,27 +1,80 @@
 let dogImgBar = document.querySelector('#dog-info');
 let dogbar = document.querySelector('#dog-bar');
+const btn = document.createElement('button')
+const filterBtn = document.querySelector('#good-dog-filter')
 
 fetch('http://localhost:3000/pups')
-.then(resp=>resp.json())
-.then(data=> {
-    data.forEach(element => {
+.then(response=>response.json())
+.then(resp=>console.log(resp))
+.then(resp=> (resp.forEach(pup =>dog(pup)))) 
+
+function dog(pup){
      
-        const img = document.createElement("img");
-        img.src = element.image;
-        dogImgBar.appendChild(img)
-/*
-        const himg = document.createElement("h4");
-        himg.innerText = "This is a test";
-        document.body.appendChild(h4)
-*/
-        let pupsName = document.createElement(h2);
-        dogImgBar.appendchild(pupsName);
-        pupsName.textContent = element.name;
+const dogSpan = document.createElement('span')
+dogbar.appendChild(dogSpan)
+
+const img = document.createElement("img");
+img.src = pup.image;
+dogSpan.appendChild(img)
+
+const pupName = document.createElement('h2')
+pupName.textContent = pup.name
+dogSpan.appendChild(pupName)
 
 
-    })  
+dogSpan.appendChild(btn)
 
-    });
+
+if (pup.isGoodDog === 'true'){ btn.textContent = "Good Dog!"}
+else {  btn.textContent = "Bad Dog!"}
+
+btn.addEventListener('click', event =>
+{
+if ( btn.textContent === "Good Dog!"){btn.textContent = "Bad Dog!"
+const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          isGoodDog: "false",
+         
+        }),
+      };
+      
+      fetch("http://localhost:3000/pups", configurationObject);}
+ else if (btn.textContent = "Bad Dog!"){btn.textContent = "Good Dog!"
+ const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          isGoodDog: "true",
+         
+        }),
+      };
+      
+      fetch("http://localhost:3000/pups", configurationObject);}
+})
+
+
+
+
+  } 
+
+
+filter.addEventListener('click', event =>{
+        if(filter.textContent ==='Filter good dogs: OFF'){filter.textContent = "Filter good dogs: ON";
+        }
+        else if(filter.textContent ==='Filter good dogs: ON'){filter.textContent = "Filter good dogs: OFF"}
+
+})
+
+
+
 
         
 
